8217baa45c0721759e2aab1c6ff5d2c462ecac1a
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-11 10:48:30 +0100

    added home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-j.png'
+
+const Home = () => {
+    
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                    <img src={LogoTitle} alt="developer" />
+                    Jiwei
+                    <br />
+                    web developer
+                </h1>
+                <h2>Full-Stack Developer / Machine Learning Expert / Cloud Engineer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home
