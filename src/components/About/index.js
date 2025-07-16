9d8e5418d78f301c0c20f1d3234593f12283b11a
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-16 12:37:35 +0100

    added info to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,22 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+
 const About = () => {
-    ('Hello')
+    return (
+        <div className="container about-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={15}
+                    />
+                    <p>I’m a driven front-end developer passionate about crafting responsive and accessible web interfaces.</p>
+                    <p>I focus on writing clean, efficient code and collaborating effectively with designers and engineers.</p>
+                    <p>I constantly learn and adapt to modern technologies to deliver high-quality digital experiences.</p>
+                </h1>
+            </div>
+        </div>
+    )
 }
 
 export default About
