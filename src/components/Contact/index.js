e9356574888a31812171cc20808349b2f9de70bb
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-17 11:59:48 +0100

    added form to contacts page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -22,6 +22,32 @@ const Contact = () => {
                     <h1>
                         <AnimatedLetters letterClass={letterClass} strArray={contactArray} idx={15}/>
                     </h1>
+                    <p>
+                        Thank you for your interest in reaching out! <br /><br />
+                        I'm happy to answer questions, hear feedback, or discuss collaboration. <br />
+                        Please use the contact form to get in touch.
+                    </p>
+                    <div className="contact-form">
+                        <form>
+                            <ul>
+                                <li className="half">
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className="half">
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li >
+                                    <input placeholder="Subject" type="text" name="subject" required />
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="message" required ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             <Loader type="pacman" />
