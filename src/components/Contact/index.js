1997e7c686a374b2a91f08bde1ff8a4f607b1157
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-17 23:27:27 +0100

    updated email sending script

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
     const contactArray = ['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']
 
     useEffect(() => {
@@ -14,6 +16,25 @@ const Contact = () => {
             return () => clearTimeout(timer)
         }, [])
 
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'service_haocbp9',
+            'template_w8s0qon',
+            refForm.current,
+            '1bubQQVXhVTFEH1Hd'
+        )
+        .then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again')
+            }
+        )
+    }
 
     return (
         <>
@@ -28,7 +49,7 @@ const Contact = () => {
                         Please use the contact form to get in touch.
                     </p>
                     <div className="contact-form">
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
                                     <input type="text" name="name" placeholder="Name" required />
