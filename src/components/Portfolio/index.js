2e43dab49ae1afa0a572920e8ea404e93f900f6d
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-18 13:32:06 +0100

    added portfolio title

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Portfolio = () => {
-    return 'hello'
+    const [letterClass, setLetterClass] = useState('text-animate')
+    
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letter={letterClass}
+                        strArray={"Portfolio".split("")} 
+                        idx={15}
+                    />
+                </h1>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
 }
 
 export default Portfolio
