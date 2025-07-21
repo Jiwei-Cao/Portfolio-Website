ccca0a3ce7babdc02dd84fe40d49dfa4fd515a5a
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-21 15:03:19 +0100

    updated home page styles

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,8 +1,5 @@
-import { useEffect, useRef } from 'react'
 import './index.scss';
 import LogoJ from '../../../assets/images/logo-j-bare.png'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import gsap from 'gsap-trial'
 
 const Logo = () => {
 
