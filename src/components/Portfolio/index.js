e9c77ea56cc6e12608e3fc813ce1381c813634b0
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-24 14:34:41 +0100

    updated portfolio component styling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import './index.scss'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import portfolioData from '../../data/portfolio.json'
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import portfolioData from "../../data/portfolio.json";
 
 const Portfolio = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    
-    useEffect(() => {
-            const timer = setTimeout(() => {
-                setLetterClass('text-animate-hover')
-            }, 3000)
-            return () => clearTimeout(timer)
-        }, [])
+    const [letterClass, setLetterClass] = useState('text-animate');
+    console.log(portfolioData);
 
-    const renderPortfolio = (portfolio) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass("text-animate-hover");
+        }, 3000); 
+
+        return () => { 
+            clearTimeout(timer);
+        }
+    });
+
+    const renderPortfolio = (portfolio) => { 
         return (
-            <div className="images-container">
+            <div className = "images-container">
                 {
                     portfolio.map((port, idx) => {
                         return (
-                            <div className="image-box" key={idx}>
-                                <img src={port.cover} className="portfolio-image" alt="portfolio"/>
+                            <div key={idx} className="image-box">
+                                <img src={port.cover} alt="portfolio" className = "portfolio-image"/>
                                 <div className="content">
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
-                                    <button className="btn" onClick={() => window.open(port.url)}>view</button>
+                                    <button className="btn" onClick={() => window.open(port.url)}>View</button>
                                 </div>
                             </div>
                         )
@@ -34,22 +38,17 @@ const Portfolio = () => {
             </div>
         )
     }
-
     return (
         <>
             <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Portfolio".split("")} 
-                        idx={15}
-                    />
+                <h1 className = "page-title">
+                    <AnimatedLetters letterClass = {letterClass} strArray={"Portfolio".split("")} idx={15}/>
                 </h1>
                 <div>{renderPortfolio(portfolioData.portfolio)}</div>
             </div>
-            <Loader type="pacman" />
+            <Loader type="pacman"/>
         </>
-    )
+    );
 }
 
 export default Portfolio
